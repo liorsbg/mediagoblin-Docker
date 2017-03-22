12d68b5db42af416a3516be822eb680f18b2c614
Lior Sabag <lior@trendiguru.com>
2017-03-22 21:38:36 +0200

    Turn off eager celery

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,14 @@
 nginx
 chown -hR mediagoblin:www-data /var/lib/mediagoblin
 cd /srv/mediagoblin.example.org/mediagoblin/
-sudo -u mediagoblin bin/gmg dbupdate
-sudo -u mediagoblin bin/gmg adduser --username admin --password CoNi4NGERsUbUMmUGHt
-sudo -u mediagoblin bin/gmg makeadmin admin
-sudo -u mediagoblin ./lazyserver.sh --server-name=fcgi fcgi_host=127.0.0.1 fcgi_port=26543
+bin/gmg dbupdate
+bin/gmg adduser --username admin --password CoNi4NGERsUbUMmUGHt
+bin/gmg makeadmin admin
+
+# ./lazyserver.sh --server-name=fcgi fcgi_host=127.0.0.1 fcgi_port=26543
+
+CELERY_ALWAYS_EAGER=false \
+ /srv/mediagoblin.example.org/mediagoblin/bin/paster serve \
+ /srv/mediagoblin.example.org/mediagoblin/paste.ini \
+ --pid-file=/var/run/mediagoblin.pid \
+ --server-name=fcgi fcgi_host=127.0.0.1 fcgi_port=26543
